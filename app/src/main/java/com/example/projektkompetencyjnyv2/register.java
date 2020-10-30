720fcac4284c059a9064a0cdb21db8f39e8a2a60
Janek <aviostpl@gmail.com>
2020-10-30 15:52:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projektkompetencyjnyv2/register.java
@@ -0,0 +1,18 @@
+
+package com.example.projektkompetencyjnyv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
